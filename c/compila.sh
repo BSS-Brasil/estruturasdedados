7bd596831f944dcdae8116dab9fe1d7fdb75e7bb
Ed de Almeida <edvaldoajunior@gmail.com>
2024-02-09 21:42:26 -0300

    compila.sh

--- c/compila.sh
+++ c/compila.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+FUNCOES=`ls $1/funcoes/*.c`
+
+for i in $FUNCOES; do
+  gcc -c $i -I $1/headers -o $1/objects/`basename $i .c`.o
+end
+
+OBJETOS=`ls $1/objects/*.o`
+
 gcc $1/main.c -I $1/headers -o $1/bin/$1 
 
 exit 0
